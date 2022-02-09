305689bf24aa31dc03c5bd4cd9aa93254f409814
skqkdld <90442799+skqkdld@users.noreply.github.com>
2022-02-10 00:46:32 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    $('.card_img').click(function () {
+        $('.modal').fadeIn();
+    });
+
+    $('.modal_but').click(function () {
+        $('.modal').fadeOut();
+    });
+
+    $('.single-item').slick({
+        dots: true,
+        infinite: true,
+        speed: 2000,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $('.autoplay').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+        ]
+    });
+
+    $('.bi-text-right').click(function () {
+        $('.toggle_menu').slideToggle();
+    });
+
+    $(window).resize(function(){
+
+        var width = window.innerWidth;
+        
+        if(width < 760){
+        
+        $('#slide1 img').attr('src', '../img/kahi-1.png')
+        
+        }
+        
+        else {$('#slide1 img').attr('../img/mobile_banner1.png');}
+        
+        }).resize();
+});
